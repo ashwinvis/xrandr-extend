58e551e59cbb8c064a09aaa3702f0468db0d2e4c
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2019-05-16 13:13:24 +0200

    Fix a typo in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ try:
 except LookupError:
     # This means the source code was not from git / PyPI
     setup(
-        version='version='0.1.0'',
+        version='0.1.0',
         **setup_kwargs
     )
 
