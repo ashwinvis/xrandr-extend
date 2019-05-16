4eb402e63484526c8989e3ba457d1514d0396bb0
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2019-05-16 11:15:27 +0200

    Add more tests

--- tests/test_xrandr_extend.py
+++ tests/test_xrandr_extend.py
@@ -6,10 +6,33 @@
 import pytest
 from contextlib import suppress
 from shlex import split
-from xrandr_extend.main import run
+from pathlib import Path
+from tempfile import gettempdir
+import os
+
+from xrandr_extend import main, config
 
 
-def test_command_line_interface(args="-h"):
+@pytest.mark.parametrize("args", [
+    "-h",
+    "--dry-run vga",
+    "--dry-run hdmi",
+    "-d --pan vga",
+    "-d --mirror vga",
+    "-d --only vga",
+])
+def test_command_line_interface(args):
     """Test the CLI."""
     with suppress(SystemExit):
-        run(split(args))
+        main.run(split(args))
+
+
+def test_write_config():
+    config.CFG_FILE = Path(gettempdir()) / "xrandr-extend.cfg"
+    if config.CFG_FILE.exists():
+        with suppress(FileExistsError):
+            config.write_defaults()
+    else:
+        config.write_defaults()
+    config.read()
+    os.remove(config.CFG_FILE)
