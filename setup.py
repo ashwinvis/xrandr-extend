2b7ab250e232be383a86407555098e99feff8584
Ashwin V. Mohanan <dev@fluid.quest>
2021-12-22 12:19:59 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ try:
 except LookupError:
     # This means the source code was not from git / PyPI
     setup(
-        version='0.2.0',
+        version='0.3.0',
         **setup_kwargs
     )
 
--- src/xrandr_extend/__init__.py
+++ src/xrandr_extend/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Ashwin Vishnu Mohanan"""
 __email__ = "ashwinvis@protonmail.com"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
